c3b29dcc48d0b7e5b707b30c7e9915fa1eaffc86
Marsychen <65822299+Marsychen@users.noreply.github.com>
2022-05-22 14:56:34 +0200

    Updated PGUA AD, added Nellis Helicopter ARR/DEP, Mod Icon added

---
+++ Mods/tech/mdc/entry.lua
@@ -0,0 +1,24 @@
+declare_plugin("vJaBoG32 MDC Mod",
+{
+
+installed 	 = true,
+dirName	  	 = current_mod_path,
+version		 = "1.1.5",
+state		 = "installed",
+
+
+shortName = 'vJaBoG32 MDC Mod',
+fileMenuName = _("vJaBoG32 MDC Mod"),
+info		  = _("This Mod alows to download the Kneeboard and Waypoints from our FPT"),
+Skins	=
+	{
+		{
+			name	= _("MDC-Mod"),
+			dir		= "Theme"
+		},
+	},
+
+}
+)
+---------------------------------------------------------------------------------------
+plugin_done()
